71178d1ed01248ae72d0911c1a8372b80b5ea843
Bob-Al-Max <borq_10@mail.ru>
2023-05-29 10:23:47 +0300

    Initial commit

---
+++ pages/posts/page.js
@@ -0,0 +1,11 @@
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      <h1>Posts</h1>
+      <p>This is the posts page.</p>
+    </>
+  );
+}
